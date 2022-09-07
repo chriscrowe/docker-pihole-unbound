acef852831394269bf1b3bef4a83ecfc9f36e61e
Markus Niemann <markus@chanoa.de>
2022-09-07 08:13:07 +0200

    Update build_and_push.sh

--- one-container/pihole-unbound/build_and_push.sh
+++ one-container/pihole-unbound/build_and_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Run this once: docker buildx create --use --name build --node build --driver-opt network=host
 PIHOLE_VER=`cat VERSION`
-docker buildx build --build-arg PIHOLE_VERSION=$PIHOLE_VER --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t biomann/docker-pihole-unbound:$PIHOLE_VER --push .
-docker buildx build --build-arg PIHOLE_VERSION=$PIHOLE_VER --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t biomann/docker-pihole-unbound:latest --push .
+docker buildx build --build-arg PIHOLE_VERSION=$PIHOLE_VER --platform linux/amd64 -t biomann/docker-pihole-unbound:$PIHOLE_VER --push .
+docker buildx build --build-arg PIHOLE_VERSION=$PIHOLE_VER --platform linux/amd64 -t biomann/docker-pihole-unbound:latest --push .
