99081df372be9969a9b95d6f121fb9b4aef31e7a
Maximilian Irro <max@irro.at>
2025-01-12 18:27:17 +0100

    Automatically update one-container image with new pihole images (#252)
    * Hardcode Docker image version so Renovate can update it
    
    * Add workflow to publish Docker image
    
    * Updating base image version to 2024.05.0
    
    * Add renovate.json (#1)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Let Renovate automerge Docker tag updates in one-container/pihole-unbound/Dockerfile
    
    * Update pihole/pihole Docker tag to v2024.07.0
    
    * Remove obsolete build script
    
    * Specifiy platforms to build image workflow step
    
    * Automatically generate a GitHub release when publishing a Docker image
    
    * Overwrite OCI Image Specification source label to enable release note fetching for Renovate and Dependabot
    
    * Build image for all platforms that the base image also supports
    
    * Update docker/build-push-action action to v6.8.0 (#3)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- one-container/pihole-unbound/build_and_push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Run this once: docker buildx create --use --name build --node build --driver-opt network=host
-PIHOLE_VER=`cat VERSION`
-docker buildx build --build-arg PIHOLE_VERSION=$PIHOLE_VER --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t cbcrowe/pihole-unbound:$PIHOLE_VER --push .
-docker buildx build --build-arg PIHOLE_VERSION=$PIHOLE_VER --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t cbcrowe/pihole-unbound:latest --push .
-
-
